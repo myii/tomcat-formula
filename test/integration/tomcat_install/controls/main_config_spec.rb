57aed93860a7fbd829d2df5982b48904d60111f1
Imran Iqbal <iqbalmy@hotmail.com>
2020-04-24 08:55:28 +0100

    test(main_config_spec): add test

---
+++ test/integration/tomcat_install/controls/main_config_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+# Overide by platform
+catalina_tmpdir = '/var/cache/tomcat/temp'
+user_and_group = 'tomcat'
+case platform[:family]
+when 'debian'
+  case platform[:name]
+  when 'debian'
+    catalina_tmpdir = '/var/cache/tomcat8/temp'
+    main_config_file = '/etc/default/tomcat8'
+    user_and_group = 'tomcat8'
+    case platform[:release]
+    when /^10/
+      catalina_tmpdir = '/var/cache/tomcat9/temp'
+      main_config_file = '/etc/default/tomcat9'
+      platform_file = 'debian-10'
+      user_and_group = 'tomcat'
+    when /^9/
+      platform_file = 'debian-9'
+    end
+  when 'ubuntu'
+    case platform[:release]
+    when /^18/
+      platform_file = 'ubuntu-1804'
+    when /^16/
+      catalina_tmpdir = '/var/cache/tomcat7/temp'
+      main_config_file = '/etc/default/tomcat7'
+      platform_file = 'ubuntu-1604'
+    end
+  end
+when 'redhat'
+  main_config_file = '/etc/default/tomcat7'
+  platform_file = 'centos-7'
+when 'fedora'
+  main_config_file = '/etc/default/tomcat7'
+  platform_file = 'fedora-31'
+when 'suse'
+  main_config_file = '/etc/default/tomcat7'
+  platform_file = 'opensuse-leap-151'
+end
+main_config_path = "/tmp/kitchen/srv/salt/file_comparison/main_config/#{platform_file}"
+main_config = file(main_config_path).content
+
+control 'Tomcat main config' do
+  title 'should contain the lines'
+
+  describe file(main_config_file) do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    its('mode') { should cmp '0644' }
+    its('content') { should include main_config }
+  end
+end
+
+control 'Tomcat Catalina temp dir' do
+  title 'should be prepared with the settings'
+
+  describe file(catalina_tmpdir) do
+    it { should be_directory }
+    it { should be_owned_by user_and_group }
+    it { should be_grouped_into user_and_group }
+    its('mode') { should cmp '0755' }
+  end
+end
