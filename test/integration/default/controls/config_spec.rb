65bbec0151829cf7bc3d8f421ba79c3e92d71b7a
Imran Iqbal <iqbalmy@hotmail.com>
2020-04-26 22:13:01 +0100

    test: merge both into single `config_spec`

--- test/integration/default/controls/config_spec.rb
+++ test/integration/default/controls/config_spec.rb
@@ -8,6 +8,9 @@ main_config_file = '/etc/sysconfig/tomcat'
 # Default values for `control 'Tomcat Catalina temp dir'`
 catalina_tmpdir = '/var/cache/tomcat/temp'
 catalina_tmpdir_user_and_group = 'tomcat'
+# Default values for `control 'Tomcat `server.xml` config'`
+conf_dir = '/etc/tomcat'
+server_xml_user_and_group = 'tomcat'
 
 # Override by platform
 case platform[:family]
@@ -15,16 +18,22 @@ when 'debian'
   main_config_file = '/etc/default/tomcat8'
   catalina_tmpdir = '/var/cache/tomcat8/temp'
   catalina_tmpdir_user_and_group = 'tomcat8'
+  conf_dir = '/etc/tomcat8'
+  server_xml_user_and_group = 'tomcat8'
   case platform_finger
   when 'debian-10'
     main_config_file = '/etc/default/tomcat9'
     catalina_tmpdir = '/var/cache/tomcat9/temp'
     catalina_tmpdir_user_and_group = 'tomcat'
+    conf_dir = '/etc/tomcat9'
+    server_xml_user_and_group = 'tomcat'
   when 'debian-9'
   when 'debian-8'
     main_config_file = '/etc/default/tomcat7'
     catalina_tmpdir = '/var/cache/tomcat7/temp'
     catalina_tmpdir_user_and_group = 'tomcat7'
+    conf_dir = '/etc/tomcat7'
+    server_xml_user_and_group = 'tomcat7'
   when 'ubuntu-18'
   when 'ubuntu-16'
   end
@@ -44,6 +53,7 @@ when 'fedora'
 when 'suse'
   case platform_finger
   when 'opensuse-15'
+    server_xml_user_and_group = 'root'
   end
 when 'linux'
   case platform_finger
@@ -76,5 +86,29 @@ control 'Tomcat Catalina temp dir' do
     it { should be_owned_by catalina_tmpdir_user_and_group }
     it { should be_grouped_into catalina_tmpdir_user_and_group }
     its('mode') { should cmp '0755' }
+  end
+end
+
+control 'Tomcat `server.xml` config' do
+  title 'should contain the lines'
+
+  server_xml_file = "#{conf_dir}/server.xml"
+  server_xml_path = '/tmp/kitchen/srv/salt/file_comparison/server_xml/'\
+    "#{platform_finger}.xml"
+  server_xml = file(server_xml_path).content
+  # Need the hostname to be used for `tomcat.cluster`
+  server_xml = server_xml.gsub(
+    'HOSTNAME_PLACEHOLDER',
+    file('/etc/hostname').content.chomp
+  )
+
+  describe file(server_xml_file) do
+    it { should be_file }
+    it { should be_owned_by server_xml_user_and_group }
+    it { should be_grouped_into server_xml_user_and_group }
+    its('mode') { should cmp '0644' }
+    unless %w[debian-9].include?(platform_finger)
+      its('content') { should include server_xml }
+    end
   end
 end
