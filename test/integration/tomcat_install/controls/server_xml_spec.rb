e33e91884584a323bb8f0b6bbd2a6f5884ad339c
Imran Iqbal <iqbalmy@hotmail.com>
2020-04-26 10:58:13 +0100

    chore: fixup only `debian-9` needs to be avoided

--- test/integration/tomcat_install/controls/server_xml_spec.rb
+++ test/integration/tomcat_install/controls/server_xml_spec.rb
@@ -65,6 +65,6 @@ control 'Tomcat `server.xml` config' do
     it { should be_owned_by server_xml_user_and_group }
     it { should be_grouped_into server_xml_user_and_group }
     its('mode') { should cmp '0644' }
-    its('content') { should include server_xml } unless %w[debian-9 debian-8].include?(platform_finger)
+    its('content') { should include server_xml } unless %w[debian-9].include?(platform_finger)
   end
 end
